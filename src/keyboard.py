ce6458a151f1db13f38c10832ab49b8f16d2db64
dyadyasawa <dyadyasawa@gmail.com>
2023-12-30 15:29:03 +0300

    Добавление в MixinKeyboard(src/keyboard.py) атрибута language и метода change_lang.

--- src/keyboard.py
+++ src/keyboard.py
@@ -2,7 +2,15 @@ from src.item import Item
 
 
 class MixinKeyboard:
-    pass
+    def __init__(self, language='EN'):
+        self.__language = language
+
+    def change_lang(self):
+        if self.__language == 'EN':
+            self.__language = 'RU'
+        elif self.__language == 'RU':
+            self.__language = 'EN'
+
 
 
 class Keyboard(Item, MixinKeyboard):
