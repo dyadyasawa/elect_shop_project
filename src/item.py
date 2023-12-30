0956c53cb0cbaa155d2794991918e52ad5afd6c1
dyadyasawa <dyadyasawa@gmail.com>
2023-12-30 15:38:48 +0300

    Добавление функции super в класс Item в src/item.py.

--- src/item.py
+++ src/item.py
@@ -8,6 +8,7 @@ class Item:
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
+        super().__init__()
         """
         Создание экземпляра класса item.
 
